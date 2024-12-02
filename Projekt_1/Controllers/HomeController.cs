49d3a17d8de0e147263ce54af9546fa6c731ec65
Ma2thuan <lecongthuan1811@gmail.com>
2024-12-02 23:17:40 +0700

    Fix bug 1

--- Projekt_1/Controllers/HomeController.cs
+++ Projekt_1/Controllers/HomeController.cs
@@ -13,6 +13,27 @@ namespace Projekt_1.Controllers
             return View();
         }
 
+        public ActionResult login()
+        {
+            return View("login");
+        }
+
+        public ActionResult chart()
+        {
+            return View("charts");
+        }
+
+        public ActionResult light_nav()
+        {
+            return View("layout-sidenav-light");
+
+        }
+
+        public ActionResult static_nav()
+        {
+            return View("layout-static");
+        }
+
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
